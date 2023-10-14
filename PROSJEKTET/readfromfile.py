f1c53fa670303669783d1b440aab3614443acfc0
Peter Haddon <phaddon@ntnu.no>
2023-10-14 23:27:11 +0200

    minor adjustments

--- PROSJEKTET/readfromfile.py
+++ PROSJEKTET/readfromfile.py
@@ -6,13 +6,14 @@ def read_input_file(file_path):
     fordelte_laster = []
     punktlaster = []
 
+    #Åpner fila
     with open(file_path, 'r') as file:
         lines = file.readlines()
         i = 0
         while i < len(lines):
             line = lines[i].strip()
 
-            #Funksjonen krever at oppsettet av inputfil følger rekkefølgen
+            #Funksjonen krever at oppsettet av inputfil følger 'riktig 'rekkefølge!
 
             if line.startswith('#Antall knutepunkt'):
                 antall_knutepunkt = int(lines[i + 1])
@@ -23,73 +24,49 @@ def read_input_file(file_path):
                 i += 2
 
             elif line.startswith('#KP-info i rekkefølge:'):
-                i+=1 # Hopp over kommentarlinje
+                i+=1 
                 for _ in range(antall_knutepunkt):
                         i += 1 
                         data = lines[i].strip().split(', ')
                         if len(data) == 4:
                             knutepunkt_info = [float(value) if value.replace('.', '', 1).isdigit() else value for value in data]
-                            knutepunkter.append(knutepunkt_info)
+                            knutepunkter.append(knutepunkt_info) #Fyller lista med data fra inputfil
 
             elif line.startswith('#Element-info i rekkefølge:'):
-                i+=1 # Hopp over kommentarlinje
-                for _ in range(antall_element): #Fikser 
+                i+=1 
+                for _ in range(antall_element): 
                         i += 1  
                         data = lines[i].strip().split(', ')
                         element_info = [float(value) if value.replace('.', '', 1).isdigit() else value for value in data]
-                        elementer.append(element_info)
+                        elementer.append(element_info) #Fyller lista med data fra inputfil
 
             elif line.startswith('#Antall fordelte laster:'):
                 antall_fordelte_laster = int(lines[i + 1])
                 i += 2
 
             elif line.startswith('#Info om de fordelte lastene i rekkefølge:'):
-                i+=1 # Hopp over kommentarlinje
+                i+=1 
                 for _ in range(antall_fordelte_laster):
                         i += 1  
                         data = lines[i].strip().split(', ')
                         if len(data) == 4:
                             fordelte_laster_info = [float(value) if value.replace('.', '', 1).isdigit() else value for value in data]
-                            fordelte_laster.append(fordelte_laster_info)
+                            fordelte_laster.append(fordelte_laster_info) #Fyller lista med data fra inputfil
 
             elif line.startswith('#Antall punktlaster:'):
                 antall_punktlaster = int(lines[i + 1])
                 i += 2
 
             elif line.startswith('#Info om punktlastene i rekkefølge:'):
-                i+=1 # Hopp over kommentarlinje
+                i+=1 
                 for _ in range(antall_punktlaster):
                         i += 1  
                         data = lines[i].strip().split(', ')
                         if len(data) == 4:
                             punktlaster_info = [float(value) if value.replace('.', '', 1).isdigit() else value for value in data]
-                            punktlaster.append(punktlaster_info)
+                            punktlaster.append(punktlaster_info) #Fyller lista med data fra inputfil
 
             else:
                     i += 1
 
-
-
-
-
     return knutepunkter, elementer, fordelte_laster, punktlaster
-
-# Eksempel på bruk:
-file_path = 'PROSJEKTET/Inputfil1.txt'
-knutepunkter, elementer, fordelte_laster, punktlaster = read_input_file(file_path)
-
-print('\nknutepunkter')
-print(len(knutepunkter))
-print(knutepunkter)
-print('\nelementer')
-print(len(elementer))
-print(elementer)
-print('\npunktlaster')
-print(len(punktlaster))
-print(punktlaster)
-print('\nfordelte_laster')
-print(len(fordelte_laster))
-print(fordelte_laster)
-print('\npunktlaster')
-print(len(punktlaster))
-print(punktlaster)
