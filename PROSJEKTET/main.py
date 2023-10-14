d7d4c01247fe208a354c568bcc9dd23e7242cb1c
Peter Haddon <phaddon@ntnu.no>
2023-10-14 23:26:22 +0200

    -

--- PROSJEKTET/main.py
+++ PROSJEKTET/main.py
@@ -1,6 +1,32 @@
 import numpy as np
+from readfromfile import read_input_file #bør dobbelsjekkes 
+from I import I #MÅ DOBBELSJEKKES 
 
-msg = "Roll a dice"
-print(msg)
 
-print(np.random.randint(1,9))
+
+#Tester funksjoner:
+file_path = 'PROSJEKTET/Inputfil1.txt'
+knutepunkter, elementer, fordelte_laster, punktlaster = read_input_file(file_path)
+
+print('\nknutepunkter')
+print(len(knutepunkter))
+print(knutepunkter)
+print('\nelementer')
+print(len(elementer))
+print(elementer)
+print('\npunktlaster')
+print(len(punktlaster))
+print(punktlaster)
+print('\nfordelte_laster')
+print(len(fordelte_laster))
+print(fordelte_laster)
+print('\npunktlaster')
+print(len(punktlaster))
+print(punktlaster)
+
+#rør:
+print(I(elementer[1])/(10**6))
+#i-profil:
+print(I(elementer[20])/(10**6))
+#box:
+print(I(elementer[22])/(10**6))
