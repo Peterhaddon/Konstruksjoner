c2f6ce920d55f911560eeed6530ad4ed17edf3f5
Peter Haddon <phaddon@ntnu.no>
2023-10-25 11:38:55 +0200

    -

--- PROSJEKTET/lastvektor.py
+++ PROSJEKTET/lastvektor.py
@@ -3,10 +3,9 @@ def lokal_lastvektor():
 
     q1 = q2 = l = 1
 
-# deler opp lasten i to trekantlaster
-fim_ende1 = (-1/20) * (q1*l**2) + (-1/30) * (q2*l**2)
-fim_ende2 = ( 1/20) * (q1*l**2) + ( 1/30) * (q2*l**2)
-
+    # deler opp lasten i to trekantlaster
+    fim_ende1 = (-1/20) * (q1*l**2) + (-1/30) * (q2*l**2)
+    fim_ende2 = ( 1/20) * (q1*l**2) + ( 1/30) * (q2*l**2)
 
 
 return()
