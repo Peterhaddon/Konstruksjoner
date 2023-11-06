e9a0b366e31b2c21412c771ba73ee9daa5c76fe2
Peter Haddon <phaddon@ntnu.no>
2023-11-06 22:26:56 +0100

    -

--- PROSJEKTET/main.py
+++ PROSJEKTET/main.py
@@ -11,37 +11,41 @@ from matriseoperasjoner import *
 from plotKrefter import *
 
 
+def main():
+    # bestemmer variabler som er nødvendige for testing av funksjoner:
+    file_path = 'PROSJEKTET/Inputfil_JACKET.txt'
+    knutepunkter, elementer, fordelte_laster, punktlaster = readfromfile(file_path)
+    kp_koordinater, elementer_kp_til_kp = format_data(knutepunkter, elementer)
+    elementlengder = lengder(knutepunkter,elementer)
+    gsm = global_stivhetsmatrise(knutepunkter, elementer, elementlengder)
+    glv = global_lastvektor(knutepunkter, elementer, elementlengder, fordelte_laster, punktlaster)
+    r = (løs_deformasjoner(gsm, glv))
+    res = S_solve(knutepunkter, elementer, elementlengder, r, fordelte_laster)
+    M_maks, N_maks = maks_krefter(elementer, elementlengder, res, fordelte_laster)
+    rotasjoner = r[::3] #Henter ut rotasjoner for hvert knutepunkt
 
-# bestemmer variabler som er nødvendige for testing av funksjoner:
-file_path = 'PROSJEKTET/Inputfil_J.txt'
-knutepunkter, elementer, fordelte_laster, punktlaster = readfromfile(file_path)
-kp_koordinater, elementer_kp_til_kp = format_data(knutepunkter, elementer)
-elementlengder = lengder(knutepunkter,elementer)
-gsm = global_stivhetsmatrise(knutepunkter, elementer, elementlengder)
-glv = global_lastvektor(knutepunkter, elementer, elementlengder, fordelte_laster, punktlaster)
-r = (løs_deformasjoner(gsm, glv))
-res = S_solve(knutepunkter, elementer, elementlengder, r, fordelte_laster)
-M_maks, N_maks = maks_krefter(elementer, elementlengder, res, fordelte_laster)
-rotasjoner = r[::3]
+    # __AVKOMMENTER DET DU ØNSKER Å KJØRE NEDENFOR__
 
-# __AVKOMMENTER DET DU ØNSKER Å KJØRE NEDENFOR__
+        # Print element og prosent av flyt
+    # print_prosent_flyt(elementer, elementlengder, res, fordelte_laster)
 
-print_prosent_flyt(elementer, elementlengder, res, fordelte_laster)
+        # Plotter diagrammer:
+    # plot_moment(elementer, elementlengder, res, fordelte_laster)
+    # plot_skjaer(elementer, elementlengder, res, fordelte_laster)
+    # plot_normal(elementer, elementlengder, res, fordelte_laster)
 
-    # Plotter diagrammer:
-# plot_moment(elementer, elementlengder, res, fordelte_laster)
-# plot_skjaer(elementer, elementlengder, res, fordelte_laster)
-# plot_normal(elementer, elementlengder, res, fordelte_laster)
+        # Printer array med [N1, Q1, M1, N2, Q2, M2] for alle elementer
+    # print("res:", np.round(res))
 
-    # Printer array med [N1, Q1, M1, N2, Q2, M2] for alle elementer
-# print("res:", np.round(res))
+        # Plot Struktur:
+    # skalering = 1
+    # numbers = 1
+    # first_index = 0
+    # fig_init, ax_init, fig_def, ax_def = setup_plots()
+    # plot_structure_def(ax_init, kp_koordinater, elementer_kp_til_kp, numbers, first_index, skalering*rotasjoner)
+    # fig_init, ax_init, fig_def, ax_def = setup_plots()
+    # plot_structure(ax_init, kp_koordinater, elementer_kp_til_kp, numbers, first_index)
+    # plt.show()
+    return()
 
-    # Plot Struktur:
-# skalering = 1
-# numbers = 1
-# first_index = 0
-# fig_init, ax_init, fig_def, ax_def = setup_plots()
-# plot_structure_def(ax_init, kp_koordinater, elementer_kp_til_kp, numbers, first_index, skalering*rotasjoner)
-# fig_init, ax_init, fig_def, ax_def = setup_plots()
-# plot_structure(ax_init, kp_koordinater, elementer_kp_til_kp, numbers, first_index)
-# plt.show()
+main()
